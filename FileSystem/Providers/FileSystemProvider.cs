f779d9d9dd1de0a5eeefbbfc8ff049879a2575ae
Kalleby Santos <kalleby.santos@hotmail.com>
2022-12-21 11:31:32 +0000

    👌 FileSystem: Refactoring FileSystem provider as a plugin component

---
+++ FileSystem/Providers/FileSystemProvider.cs
@@ -0,0 +1,83 @@
+using CloudIn.Core.Domain.Contracts.Providers.FileSystemProvider;
+using CloudIn.Plugin.FileSystem.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace CloudIn.Plugin.FileSystem.Providers;
+
+public class FileSystemProvider : IFileSystemProvider, IAsyncDisposable
+{
+    private readonly DirectoryInfo _rootPath;
+
+    private FileStream FileStream { get; set; } = default!;
+
+    public FileSystemProvider(IOptions<FileSystemOptions> options)
+    {
+        _rootPath = new DirectoryInfo(options.Value.SavePath);
+        if (!_rootPath.Exists)
+            _rootPath.Create();
+    }
+
+    public Task RemoveAsync(string filepath)
+    {
+        var fileInfo = new FileInfo(filepath);
+
+        if (!fileInfo.Exists)
+        {
+            throw new FileNotFoundException("The file has already been removed.");
+        }
+
+        fileInfo.Delete();
+
+        return Task.CompletedTask;
+    }
+
+    public async Task<string> WriteAsync(IWriteFileValues fileValues)
+    {
+        var directory =
+            _rootPath.EnumerateDirectories(fileValues.FilePath).FirstOrDefault()
+            ?? _rootPath.CreateSubdirectory(fileValues.FilePath);
+
+        if (directory == null)
+            throw new DirectoryNotFoundException();
+
+        var fileFolder =
+            directory.EnumerateDirectories(fileValues.FileName).FirstOrDefault()
+            ?? directory.CreateSubdirectory(fileValues.FileName);
+
+        var filePath = Path.Combine(fileFolder.FullName, "file");
+        var fileName = Path.ChangeExtension(filePath, fileValues.Extension);
+
+        var file = new FileInfo(fileName);
+
+        FileStream = file.Create();
+
+        await fileValues.Content.CopyToAsync(FileStream);
+
+        await FileStream.FlushAsync();
+
+        var virtualPath = Path.GetRelativePath(_rootPath.FullName, file.FullName);
+
+        return virtualPath;
+    }
+
+    public async Task<FileStream> OpenReadAsync(string virtualPath)
+    {
+        await Task.Yield();
+
+        var physicalPath = Path.Combine(_rootPath.FullName, virtualPath);
+
+        var fileInfo = new FileInfo(physicalPath);
+
+        if (!fileInfo.Exists)
+            throw new FileNotFoundException();
+
+        FileStream = fileInfo.OpenRead();
+
+        return FileStream;
+    }
+
+    ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        return FileStream.DisposeAsync();
+    }
+}
